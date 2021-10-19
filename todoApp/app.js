f93c43eca2e6ef9d7383f22a7b2365690dd102d9
kukwa clovis <kukwaclovisngong3@gmail.com>
2021-10-19 14:13:28 +0100

    complete todoApp ready

--- todoApp/app.js
+++ todoApp/app.js
@@ -9,6 +9,16 @@ let password = document.querySelector('#password');
 let todoHead = document.querySelector('.todo-head');
 let todoBody = document.querySelector('.todo-body');
 let main = document.querySelector('.main');
+let errormsg = document.getElementById('error')
+let todoCount = document.getElementById('count')
+let logInErroMsg = document.querySelector('.errormsg')
+
+inputItem.addEventListener('keyup', () => {
+    if (inputItem.value.length > 0 && inputItem.value.trim() !== 0) {
+        errormsg.style.display = 'none'
+    }
+
+})
 
 
 menubtn.addEventListener('click', () => {
@@ -18,17 +28,34 @@ displayTodo();
 
 function addTodo() {
     let userData = inputItem.value;
-    let getFromLocalStorage = localStorage.getItem('new todo');
-    if (getFromLocalStorage == null) {
-        todoItems = [];
+    if (userData.trim().length !== 0 && userData !== "") {
+        let getFromLocalStorage = localStorage.getItem('new todo');
+        if (getFromLocalStorage == null) {
+            todoItems = [];
+        } else {
+            todoItems = JSON.parse(getFromLocalStorage); //coverting the string into json object
+        }
+
+        todoItems.push(userData)
+        localStorage.setItem('new todo', JSON.stringify(todoItems)); //transforming the json object into string
+        displayTodo();
+        errormsg.style.display = 'none'
     } else {
-        todoItems = JSON.parse(getFromLocalStorage); //coverting the string into json object
+        errormsg.style.display = 'block'
     }
 
-    todoItems.push(userData)
-    localStorage.setItem('new todo', JSON.stringify(todoItems)); //transforming the json object into string
+    todoCount.innerText = todoItems.length;
+
+}
+
+function removeAllTodos() {
+    let getFromLocalStorage = localStorage.getItem('new todo');
+    todoItems = JSON.parse(getFromLocalStorage)
+    todoItems = [];
+    localStorage.setItem("new todo", JSON.stringify(todoItems)) //updating the local storage
     displayTodo();
 }
+
 
 function displayTodo() {
     // let userData = inputItem.value;
@@ -39,43 +66,67 @@ function displayTodo() {
         todoItems = JSON.parse(getFromLocalStorage);
     }
     let items = '';
-    todoItems.forEach((element,day,hour,mins,secs,month,amp,newDate) => {
-       newDate = new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'});
+    todoItems.forEach((element, index, day, hour, mins, secs, month, amp, newDate) => {
+        newDate = new Date().toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric'
+        });
         let time = new Date();
-        amp = (hour >= 12 ) ? "PM" : "AM";
-       mins = time.getMinutes();
-       hour = time.getHours()
-       secs = time.getSeconds()
-       month = time.getMonth()
-       day = time.getDay()
+        amp = (hour >= 12) ? "PM" : "AM";
+        mins = time.getMinutes();
+        hour = time.getHours()
+        secs = time.getSeconds()
+        month = time.getMonth()
+        day = time.getDay()
 
-        items += `<li> <p> <i class="fas fa-calendar-alt" id="calendar"></i>${newDate}, ${hour}: ${mins}:${secs}</p> <p title="${element}">${element}</p><span><i class="fa fa-trash"></i></span></li>`
+        items += `<li> <p> <i class="fas fa-calendar-alt" id="calendar"></i>${newDate}, ${hour}: ${mins}:${secs}</p> <p title="${element}">${element}</p><span onclick = "removeTask(${index})"><i class="fa fa-trash"></i></span></li>`
     });
     showItems.innerHTML = items;
     inputItem.value = "";
+    todoCount.innerText = todoItems.length;
 }
 
+// creating remove task function
+function removeTask(index) {
+    let getFromLocalStorage = localStorage.getItem('new todo')
+    todoItems = JSON.parse(getFromLocalStorage)
+    todoItems.splice(index, 1); //detete that item you've choosen
+    localStorage.setItem('new todo', JSON.stringify(todoItems)) //updating the local storage after deleting as item
+    displayTodo();
+}
+
+username.addEventListener('keyup', () => {
+    logInErroMsg.style.display = 'none'
+})
+
+password.addEventListener('keyup', () => {
+    logInErroMsg.style.display = 'none'
+})
 // attaching an event listerner to the log in button
 logInBtn.addEventListener('click', () => {
-    if( username.value === "kukwa clovis" && password.value === "herald"){
+    if (username.value === "kukwa clovis" && password.value === "herald") {
         showcase.classList.add('hide-showcase');
         // todoHead.classList.add('show-todo-head');
         // todoBody.classList.add('show-todo-body');
         main.classList.add('show-main');
         username.value = "";
         password.value = "";
-    } else if(username === "kukwa clovis" && password !== "herald"){
+    } else if (username === "kukwa clovis" && password !== "herald") {
         password.classList.toggle("wrong-password");
-        
-    }else if(username !== "kukwa clovis" && password === "herald"){
-        username.classList.toggle('wrong-name');
+        logInErroMsg.style.display = 'block'
 
-    } else{
-            username.classList.toggle('wrong-info')
+    } else if (username !== "kukwa clovis" && password === "herald") {
+        username.classList.toggle('wrong-name');
+        logInErroMsg.style.display = 'block'
+
+    } else {
+        username.classList.toggle('wrong-info')
         password.classList.toggle('wrong-info')
-        
+        logInErroMsg.style.display = 'block'
     }
 })
+
 
 // window.location.href = "url?videoid = dkfldfjlfk" (javascript routing)
 /*
